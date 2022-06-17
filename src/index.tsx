23b0f1b424e0b6a3cf88a858323174132a794b4e
AmirAhmadzadeh <ahamadzadeh1378@gmail.com>
2022-06-17 13:22:22 +0430

    chore:add some base components and icons

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,35 @@
 /* @refresh reload */
-import { render } from 'solid-js/web';
+import { render } from "solid-js/web";
 
-import './index.css';
-import App from './App';
+import "./index.css";
+import App from "./App";
+import { Provider, useStore } from "./store/";
+import { Router, Route, Routes, useNavigate } from "solid-app-router";
+import Auth from "./screens/Auth";
 
-render(() => <App />, document.getElementById('root') as HTMLElement);
+const AppWithAuth = () => {
+  const nav = useNavigate();
+  const [{ token }] = useStore();
+
+  if (!token?.length) {
+    nav("/auth");
+  }
+
+  return (
+    <Routes>
+      <Route path="/" element={<App />} />
+      <Route path="/auth" element={<Auth />} />
+    </Routes>
+  );
+};
+
+render(
+  () => (
+    <Provider>
+      <Router>
+        <AppWithAuth />
+      </Router>
+    </Provider>
+  ),
+  document.getElementById("root") as HTMLElement
+);
