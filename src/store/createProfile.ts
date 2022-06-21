67873f302840a8cd82d8769ce69afcdbc7256007
AmirAhmadzadeh <ahamadzadeh1378@gmail.com>
2022-06-21 23:30:00 +0430

    chore: add attached token signal to profile resource

--- src/store/createProfile.ts
+++ src/store/createProfile.ts
@@ -1,23 +1,38 @@
-import { createResource } from "solid-js";
+import { createResource, createSignal, batch } from "solid-js";
 import type { Resource } from "solid-js";
 import { ProfileType } from "../types/profile";
 import { Actions } from ".";
 import { ProfileAgent } from "./agent/profile-agent/ProfileAgent";
+import { StoreType } from "../types/store";
+import { SetStoreFunction } from "solid-js/store";
 
 export interface ProfileActions {
   logout(): Promise<void>;
+  loadProfile(value: string | null): void;
 }
 
 export default function createProfile(
   actions: Actions,
-  agent: ProfileAgent
+  agent: ProfileAgent,
+  setState: SetStoreFunction<StoreType>
 ): Resource<ProfileType | undefined> {
-  const [profile] = createResource(() => agent.fetchProfile());
+  const [token, setToken] = createSignal<string | null>(null);
+  const [profile, { mutate }] = createResource(token, () =>
+    agent.fetchProfile()
+  );
 
   Object.assign<Actions, ProfileActions>(actions, {
+    loadProfile(value: string | null) {
+      //console.log("in load profile", value, token());
+      setToken(value);
+    },
     logout() {
-      window.location.replace("/auth");
-      localStorage.clear();
+      batch(() => {
+        setState({ token: undefined });
+        mutate(undefined);
+        window.location.replace("/auth");
+        localStorage.clear();
+      });
       return agent.logout();
     },
   });
