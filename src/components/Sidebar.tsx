1c02bd2903ff75d4a3004adb3b7f9389d9aa4920
AmirAhmadzadeh <ahamadzadeh1378@gmail.com>
2022-06-21 23:28:16 +0430

    chore: fix direct usage of signals

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,23 +1,47 @@
-import { NavLink } from "solid-app-router";
-import { Component, For } from "solid-js";
+import { Component, createComputed, createSignal, lazy, Show } from "solid-js";
+import { useStore } from "../store";
+import ChannelList from "./ChannelList";
+import Search from "./Search";
 import styles from "./styles/Sidebar.module.css";
+import { classNames } from "./UI/utils/classNames";
 import UserInfo from "./UserInfo";
-import UsersList from "./UsersList";
+const DirectsList = lazy(() => import("./DirectsList"));
 
 const Sidebar: Component = () => {
+  const [{ profile }, { logout, loadDirects }] = useStore();
+  const [store] = useStore();
+  const [directsListLoaded, setDirectListLoaded] = createSignal(false);
+
+  createComputed(() => {
+    !store.directs.length &&
+      loadDirects(store?.profile?.email ? store?.profile?.email : null);
+    store.directs.length && setDirectListLoaded(true);
+  });
+
+  //TODO : fix load problem in directs list
   return (
     <div class={styles.Nav}>
-      <UserInfo />
+      <UserInfo
+        displayName={profile?.displayName || ""}
+        image={profile?.image || ""}
+      >
+        <button
+          class={classNames(styles.text, styles.button)}
+          onClick={() => logout()}
+        >
+          logout
+        </button>
+      </UserInfo>
       <nav class={styles.ChannelNav}>
-        <For each={["awesome", "general"]}>
-          {(title: string) => (
-            <NavLink activeClass={styles.active} href={`/channel/${title}`}>
-              # {title}
-            </NavLink>
-          )}
-        </For>
+        <ChannelList />
         <br />
-        <UsersList />
+        <div class={styles.List}>
+          <h2> users </h2>
+          <Search />
+          <Show when={directsListLoaded()}>
+            <DirectsList />
+          </Show>
+        </div>
       </nav>
     </div>
   );
