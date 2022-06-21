0f9537cf7d47818f4957b5944396dfc4cc6b4b49
AmirAhmadzadeh <ahamadzadeh1378@gmail.com>
2022-06-21 23:31:00 +0430

    chore: attach directs soruce singal to directs resource

--- src/store/createDirects.ts
+++ src/store/createDirects.ts
@@ -1,23 +1,33 @@
-import { createResource } from "solid-js";
+import { createResource, createSignal } from "solid-js";
 import type { Resource } from "solid-js";
 import { DirectType } from "../types/direct";
 import { Actions } from ".";
 import { DirectAgent } from "./agent/direct-agent/DirectAgent";
+import { StoreType } from "../types/store";
 
 export interface DirectActions {
-  addToDirects(user: string): void;
+  addToDirects(email: string): void;
+  loadDirects(value: string | null): void;
 }
 
 export default function createDirects(
+  state: StoreType,
   actions: Actions,
   agent: DirectAgent
 ): Resource<DirectType | undefined> {
-  const [directs, { refetch }] = createResource(() => agent.fetchDirects());
+  const [directsSource, setDirectsSource] = createSignal();
+  const [directs] = createResource(directsSource, () => agent.fetchDirects());
 
   Object.assign<Actions, DirectActions>(actions, {
-    addToDirects: async (user: string) => {
-      await agent.addToDirects(user);
-      refetch();
+    loadDirects(value: string | null) {
+      setDirectsSource(value);
+    },
+    async addToDirects(email: string) {
+      if (state.directs?.findIndex((direct) => direct.email == email) == -1) {
+        console.log("email is ", email, directsSource());
+        setDirectsSource(email);
+        await agent.addToDirects(email);
+      }
     },
   });
 
