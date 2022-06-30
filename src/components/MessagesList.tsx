d1d7f90457f7d8120f6870f92287adeb0234c31e
AmirAhmadzadeh <ahamadzadeh1378@gmail.com>
2022-06-30 17:41:56 +0430

    feat: MessagesList component

---
+++ src/components/MessagesList.tsx
@@ -0,0 +1,35 @@
+import { Component, For } from "solid-js";
+import { useStore } from "../store";
+import { MessageWithAvatar } from "./Chat";
+import styles from "./styles/Chat.module.css";
+
+const MessagesList: Component = () => {
+  const [store] = useStore();
+
+  return (
+    <>
+      <div>
+        <div class={styles.Day}>
+          <div class={styles.DayLine} />
+          <div class={styles.DayText}>12/6/2018</div>
+          <div class={styles.DayLine} />
+        </div>
+      </div>
+      <For each={store.direct}>
+        {(message) => {
+          return (
+            <MessageWithAvatar
+              image={message.from.image}
+              username={message.from.displayName}
+              disabled={message._id === "Pending"}
+            >
+              {message.content}
+            </MessageWithAvatar>
+          );
+        }}
+      </For>
+    </>
+  );
+};
+
+export default MessagesList;
