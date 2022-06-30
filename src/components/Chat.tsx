b3d574108e1f9adf6c609277548d1c41a30cae54
AmirAhmadzadeh <ahamadzadeh1378@gmail.com>
2022-06-30 17:40:04 +0430

    chore: disabled prop for pending chat items

--- src/components/Chat.tsx
+++ src/components/Chat.tsx
@@ -3,18 +3,21 @@ import styles from "./styles/Message.module.css";
 import { classNames } from "./UI/utils/classNames";
 
 export const MessageWithAvatar: Component<
-  ParentProps<{ username?: string; image?: string }>
-> = ({ children, username, image }) => {
+  ParentProps<{ username?: string; image?: string; disabled: boolean }>
+> = ({ children, username, image, disabled }) => {
   return (
     <div class={classNames(styles.Message, styles.withAvatar)}>
-      <img class={styles.Avatar} src={image || ""} />
+      <img class={styles.Avatar} src={image || "https://i.pravatar.cc/300"} />
       <div class={styles.Author}>
         <div>
           <span class={styles.UserName}>{username || ""}</span>
           {/* FIXME: 3:37Pm fix this*/}
           <span class={styles.TimeStamp}>3:37 PM</span>
         </div>
-        <div class={styles.MessageContent}> {children}</div>
+        <div aria-disabled={`${disabled}`} class={styles.MessageContent}>
+          {children}
+        </div>
+        {disabled && "DISABLED"}
       </div>
     </div>
   );
