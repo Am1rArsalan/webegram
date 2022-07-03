e8b5b399a322ca15a8e0236ec60f28fddb0decbc
AmirAhmadzadeh <ahamadzadeh1378@gmail.com>
2022-07-03 08:54:35 +0430

    feat: add generator for direct type

--- src/types/direct.ts
+++ src/types/direct.ts
@@ -1,12 +1,31 @@
+import { CreateDirectDto } from "../dtos/Direct";
+
 export type DirectType = {
   _id: string;
-  from: string;
-  to: {
+  from: {
     _id: string;
     displayName: string;
     email: string;
+    image: string;
   };
+  to: string;
   content: string;
   created_at: string;
   updated_at: string;
 };
+
+export function newDirect(data: CreateDirectDto): DirectType {
+  return {
+    _id: "Pending",
+    from: {
+      _id: data.userId,
+      displayName: data.displayName,
+      email: data.email,
+      image: data.image,
+    },
+    to: data.to,
+    content: data.content,
+    created_at: data.creationTime,
+    updated_at: data.creationTime,
+  };
+}
