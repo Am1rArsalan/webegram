bb77f39ad4ac2a91b2c51fd09d91e9573b2cf31f
AmirAhmadzadeh <ahamadzadeh1378@gmail.com>
2022-07-11 11:13:36 +0430

    chore: update direct type

--- src/types/store.ts
+++ src/types/store.ts
@@ -1,4 +1,3 @@
-import { DirectType } from "./direct";
 import { DirectsType } from "./directs";
 import { ProfileType } from "./profile";
 import { UserType } from "./user";
@@ -7,7 +6,6 @@ export type StoreType = {
   readonly token: string | null;
   readonly profile: ProfileType | undefined;
   readonly users: UserType[];
-  readonly directs: DirectsType | undefined;
-  readonly direct: DirectType[];
+  readonly directs: DirectsType;
   readonly socketConnection: boolean;
 };
