c3d1d220832b55b728f8d9d709e93f0ca81c39bc
AmirAhmadzadeh <ahamadzadeh1378@gmail.com>
2022-07-11 11:25:49 +0430

    feat: add fetchDirectChats agent and update agents

--- src/store/agent/directs-agent/DirectsAgent.ts
+++ src/store/agent/directs-agent/DirectsAgent.ts
@@ -1,9 +1,11 @@
-import { DirectsType } from "../../../types/directs";
+//TODO : fix any
+import { DirectApiType, DirectsApiType } from "../../../types/directs";
 import { BaseAgentImpl } from "../BaseAgent";
 
 export interface DirectsAgent {
-  fetchDirects(): Promise<DirectsType | undefined>;
-  addToDirects(id: string): Promise<void | undefined>;
+  fetchDirects(): Promise<DirectsApiType | undefined>;
+  addToDirects(id: string): Promise<DirectApiType | undefined>;
+  fetchDirectChats(id: string): Promise<any[] | undefined>;
 }
 
 export class DirectsAgentImpl extends BaseAgentImpl implements DirectsAgent {
@@ -12,18 +14,22 @@ export class DirectsAgentImpl extends BaseAgentImpl implements DirectsAgent {
   }
 
   addToDirects(id: string) {
-    return this.createFetchHttpRequest<void>(
+    return this.createFetchHttpRequest<DirectApiType>(
       "PUT",
-      `/user/invite?email=${id}`,
+      `/direct/invite?id=${id}`,
       "data"
     );
   }
 
   fetchDirects() {
-    return this.createFetchHttpRequest<DirectsType>(
+    return this.createFetchHttpRequest<DirectsApiType>(
       "GET",
-      "/user/directs",
+      "/direct/directs",
       "data"
     );
+  }
+
+  fetchDirectChats(id: string) {
+    return this.createFetchHttpRequest<any[]>("GET", `/chat?id=${id}`, "data");
   }
 }
