c036dbcff25e4e626e7f636f2bfc504c2e70bd0b
AmirAhmadzadeh <ahamadzadeh1378@gmail.com>
2022-07-11 11:14:57 +0430

    chore: update props

--- src/components/Chat.tsx
+++ src/components/Chat.tsx
@@ -2,9 +2,16 @@ import { Component, ParentProps } from "solid-js";
 import styles from "./styles/Message.module.css";
 import { classNames } from "./UI/utils/classNames";
 
+type MessageWithAvatarProps = {
+  username?: string;
+  image?: string;
+  disabled: boolean;
+  createdAt: string;
+};
+
 export const MessageWithAvatar: Component<
-  ParentProps<{ username?: string; image?: string; disabled: boolean }>
-> = ({ children, username, image, disabled }) => {
+  ParentProps<MessageWithAvatarProps>
+> = ({ children, username, image, createdAt, disabled }) => {
   return (
     <div class={classNames(styles.Message, styles.withAvatar)}>
       <img class={styles.Avatar} src={image || "https://i.pravatar.cc/300"} />
@@ -12,7 +19,7 @@ export const MessageWithAvatar: Component<
         <div>
           <span class={styles.UserName}>{username || ""}</span>
           {/* FIXME: 3:37Pm fix this*/}
-          <span class={styles.TimeStamp}>3:37 PM</span>
+          <span class={styles.TimeStamp}>{createdAt}</span>
         </div>
         <div aria-disabled={`${disabled}`} class={styles.MessageContent}>
           {children}
