9d34de197fda72fa882499917ae8322adcede346
AmirAhmadzadeh <ahamadzadeh1378@gmail.com>
2022-07-11 11:44:33 +0430

    chore: adding token check into app component

--- src/screens/App.tsx
+++ src/screens/App.tsx
@@ -1,14 +1,20 @@
-import { Outlet } from "solid-app-router";
+import { Outlet, useNavigate } from "solid-app-router";
 import { Component, lazy, onCleanup, Show } from "solid-js";
 import { useStore } from "../store";
 import styles from "./styles/App.module.css";
+
 const Sidebar = lazy(() => import("../components/Sidebar"));
 
 const App: Component = () => {
+  const nav = useNavigate();
   const [store, { resetSocketConnection }] = useStore();
 
+  if (!store.token) {
+    nav("/auth");
+  }
+
   onCleanup(() => {
-    resetSocketConnection();
+    store.socketConnection && resetSocketConnection();
   });
 
   return (
@@ -16,11 +22,9 @@ const App: Component = () => {
       <Sidebar />
       <Show
         when={store.socketConnection}
-        fallback={
-          <h2 style={{ position: "fixed", bottom: "10px" }}>connecting...</h2>
-        }
+        fallback={<h2 class={styles.socketConnection}>connecting...</h2>}
       >
-        <h2 style={{ position: "fixed", bottom: "10px" }}> connected </h2>
+        <h2 class={styles.socketConnection}> connected </h2>
       </Show>
       <Outlet />
     </div>
