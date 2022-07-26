88a0cfa494ae9e4386bcfd1edd85a118e3e77163
AmirAhmadzadeh <ahamadzadeh1378@gmail.com>
2022-07-26 17:40:22 +0430

    chore: add plus icon

---
+++ src/components/UI/icons/Plus.tsx
@@ -0,0 +1,19 @@
+import { Component } from "solid-js";
+import { IconProps } from "./IconProps";
+
+export const Plus: Component<IconProps> = (props) => {
+  const { width, height, fill, ...rest } = props;
+
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 6 6"
+      {...rest}
+      width={width}
+      height={height}
+      fill={fill}
+    >
+      <path d="M3.75 0L2.25 0 2.25 2.25 0 2.25 0 3.75 2.25 3.75 2.25 6 3.75 6 3.75 3.75 6 3.75 6 2.25 3.75 2.25z" />
+    </svg>
+  );
+};
