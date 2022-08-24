477129f85a07014af825cff20f573f41f4e20b68
AmirAhmadzadeh <ahamadzadeh1378@gmail.com>
2022-08-25 00:14:16 +0430

    fix: ts type error

--- src/components/DirectChat.tsx
+++ src/components/DirectChat.tsx
@@ -4,10 +4,21 @@ import styles from "./styles/Chat.module.css";
 import MessagesList from "./MessagesList";
 import ChatInputForm from "./ChatInputForm";
 import { useStore } from "../store";
+import { MessageType } from "../types/message";
 
 function DirectChat() {
   const params = useParams();
   const [store] = useStore();
+
+  function getMessages() {
+    const room = store.directs.get(`${params.email}@gmail.com`);
+
+    if (room) {
+      return room.chats;
+    }
+
+    return new Map<string, MessageType[]>();
+  }
 
   return (
     <div class={styles.ChatContainer}>
@@ -17,10 +28,11 @@ function DirectChat() {
         </div>
         <div class={styles.Messages} id="ChatMain">
           <div class={styles.EndOfMessages}>{"That's every message!"}</div>
-          <Show when={store.directs.get(`${params.email}@gmail.com`)}>
-            <MessagesList
-              messages={store.directs?.get(`${params.email}@gmail.com`)?.chats}
-            />
+          <Show
+            when={store.directs.has(`${params.email}@gmail.com`)}
+            fallback="loading..."
+          >
+            <MessagesList messages={getMessages()} />
           </Show>
         </div>
         <ChatInputForm />
