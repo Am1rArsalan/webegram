f343b56554374d9f7ee73ddf52238dcd7a980623
AmirAhmadzadeh <ahamadzadeh1378@gmail.com>
2022-08-29 22:51:58 +0430

    chore: extract Profile component

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -2,65 +2,34 @@ import { useStore } from "../store";
 import ChannelList from "./ChannelList";
 import Search from "./Search";
 import styles from "./styles/Sidebar.module.css";
-import UserInfo from "./UserInfo";
 import DirectsList from "./DirectsList";
-import { Button, IconButton } from "./UI/button";
-import { Plus } from "./UI/icons/Plus";
-import { Popover } from "solid-popover";
-import AddGroupForm from "./AddGroupForm";
-import { createSignal } from "solid-js";
+import Profile from "./Profile";
+import { useNavigate } from "solid-app-router";
 
-function Sidebar(props: { popoverContainerRef?: HTMLDivElement }) {
-  const [store, { logout }] = useStore();
-  const [isOpen, setIsOpen] = createSignal(false);
-  const togglePopover = () => setIsOpen(!isOpen());
+function Sidebar() {
+  const [_, { addToDirects }] = useStore();
+  const nav = useNavigate();
+  let popoverContainerRef;
 
   return (
-    <>
-      <div
-        style={{
-          width: "90%",
-          margin: "1rem auto",
-          display: "flex",
-          "justify-content": "center",
-        }}
-      >
-        <UserInfo
-          displayName={store.profile?.displayName || ""}
-          image={store.profile?.image || ""}
-        >
-          <div class={styles.NavUser}>
-            <Button class={styles.textButton} onClick={() => logout()}>
-              logout
-            </Button>
-            <Popover
-              parentElement={props.popoverContainerRef}
-              isOpen={isOpen()}
-              content={<AddGroupForm closeForm={togglePopover} />}
-              onClickOutside={() => setIsOpen(false)}
-              positions={["bottom", "right", "top", "left"]}
-              spacing={10}
-            >
-              <IconButton
-                onClick={togglePopover}
-                class={styles.addChannelButton}
-              >
-                <Plus fill="#000" width={11} height={11} />
-              </IconButton>
-            </Popover>
-          </div>
-        </UserInfo>
-      </div>
+    <div class={styles.Nav} ref={popoverContainerRef}>
+      <Profile popoverContainerRef={popoverContainerRef} />
       <nav class={styles.ChannelNav}>
         <ChannelList />
         <br />
         <div class={styles.List}>
           <h2> users </h2>
-          <Search />
+          <Search
+            action={(data) => {
+              addToDirects(data.email, data._id);
+              nav(`user/${data.email.split("@")[0]}`);
+            }}
+            containerRef={popoverContainerRef}
+          />
           <DirectsList />
         </div>
       </nav>
-    </>
+    </div>
   );
 }
 
