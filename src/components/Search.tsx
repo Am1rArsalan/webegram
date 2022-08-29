374b626307040182c5321a65be1e234a02dbc559
AmirAhmadzadeh <ahamadzadeh1378@gmail.com>
2022-08-29 22:38:32 +0430

    feat: make search user reusable

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -1,41 +1,73 @@
-import { useNavigate } from "solid-app-router";
 import { createSignal, For } from "solid-js";
+import { Popover } from "solid-popover";
 import { useStore } from "../store";
+import { UserType } from "../types/user";
+import styles from "./styles/Search.module.css";
 
-function Search() {
+type Props = {
+  action: (user: UserType) => void;
+  containerRef: HTMLElement | undefined;
+};
+
+function Search(props: Props) {
   const [value, setValue] = createSignal("");
-  const [store, { updateSearchQuery, addToDirects, resetSearchedUsers }] =
-    useStore();
-  const nav = useNavigate();
+  const [store, { updateSearchQuery, resetSearchedUsers }] = useStore();
+
+  let searchInputRef: HTMLInputElement | undefined;
+
+  function isPopoverOpen() {
+    return value().length > 0 && store.users.length > 0;
+  }
 
   return (
-    <ul>
+    <Popover
+      isOpen={isPopoverOpen()}
+      positions={["bottom", "right", "left", "top"]}
+      parentElement={props.containerRef}
+      spacing={10}
+      containerStyle={{
+        background: "white",
+        border: "1px solid #eee",
+        padding: ".4rem 0",
+        borderRadius: "4px",
+      }}
+      content={
+        <ul class={styles.UsersList}>
+          <For each={store.users}>
+            {(user) => {
+              return (
+                <>
+                  <li
+                    onClick={() => {
+                      props.action(user);
+                      setValue("");
+                      resetSearchedUsers();
+                    }}
+                    class={styles.UserListItem}
+                  >
+                    <span>
+                      <img src={user.image} />
+                    </span>
+                    <span>{user.email}</span>
+                  </li>
+                  <div class={styles.Divider} />
+                </>
+              );
+            }}
+          </For>
+        </ul>
+      }
+    >
       <input
+        ref={searchInputRef}
+        class={styles.SearchInput}
         value={value()}
         onInput={(ev) => {
           setValue(ev.currentTarget.value);
           updateSearchQuery(ev.currentTarget.value);
         }}
       />
-      {value()}
-      <br />
-      <For each={store.users.filter((item) => !store.directs.has(item.email))}>
-        {(user) => {
-          return (
-            <li
-              onClick={() => {
-                addToDirects(user.email, user._id);
-                setValue("");
-                resetSearchedUsers();
-                nav(`user/${user.email.split("@")[0]}`);
-              }}
-            >
-              user : {user?.email}
-            </li>
-          );
-        }}
-      </For>
-    </ul>
+    </Popover>
   );
 }
 
