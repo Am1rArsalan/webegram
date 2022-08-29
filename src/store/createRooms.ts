1b3956b42a78f06bd46bd706e007e07229e686f8
AmirAhmadzadeh <ahamadzadeh1378@gmail.com>
2022-08-29 22:47:52 +0430

    feat: add createRoom and addMember in room store

--- src/store/createRooms.ts
+++ src/store/createRooms.ts
@@ -1,12 +1,13 @@
-import { Actions } from ".";
+import { createResource, createSignal } from "solid-js";
 import { StoreType } from "../types/store";
 import { RoomsAgent } from "./agent/rooms-agent/RoomsAgent";
-import { RoomType } from "../types/rooms";
-import { createResource, createSignal } from "solid-js";
+import { AddMemberDto } from "../dtos/Room";
+import { Actions } from ".";
 
 export interface RoomActions {
-  createRoom(groupName: string, slug: string): Promise<RoomType>;
+  createRoom(groupName: string, slug: string): void;
   loadRooms(value: string | null): void;
+  addMember(data: AddMemberDto): void;
 }
 
 export default function createRooms(
@@ -15,7 +16,7 @@ export default function createRooms(
   agent: RoomsAgent
 ) {
   const [roomsSource, setRoomsSource] = createSignal();
-  const [rooms] = createResource(
+  const [rooms, { mutate }] = createResource(
     roomsSource,
     async () => await agent.fetchRooms()
   );
@@ -24,8 +25,39 @@ export default function createRooms(
     loadRooms(value: string | null) {
       return setRoomsSource(value);
     },
-    createRoom(groupName: string, slug: string) {
-      return agent.createRoom({ name: groupName, slug });
+    async createRoom(groupName: string, slug: string) {
+      if (!state.profile) return;
+      let createdRoom = await agent.createRoom({ name: groupName, slug });
+      createdRoom = { ...createdRoom, members: [state.profile] };
+      mutate((prev) => (prev ? [...prev, createdRoom] : [createdRoom]));
+    },
+    async addMember(data: AddMemberDto) {
+      const prev = [...state.rooms];
+
+      const foundedRoomIndex = prev.findIndex((room) => {
+        return room.slug == data.roomSlug;
+      });
+
+      if (foundedRoomIndex == -1) return;
+
+      if (
+        prev[foundedRoomIndex].members.findIndex(
+          (item) => item._id === data.memberId
+        ) != -1
+      )
+        return;
+
+      await agent.addMember(data);
+      const foundedRoom = { ...prev[foundedRoomIndex] };
+      foundedRoom.members = [
+        ...prev[foundedRoomIndex].members,
+        {
+          _id: data.memberId,
+          displayName: data.memberName,
+        },
+      ];
+      prev[foundedRoomIndex] = foundedRoom;
+      mutate(prev);
     },
   });
 
