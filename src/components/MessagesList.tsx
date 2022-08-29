830c6e6018b364699179824cd0e5263e73ec9db7
AmirAhmadzadeh <ahamadzadeh1378@gmail.com>
2022-08-29 22:54:12 +0430

    chore: fix ts error

--- src/components/MessagesList.tsx
+++ src/components/MessagesList.tsx
@@ -1,6 +1,6 @@
 import dayjs from "dayjs";
 import { useParams } from "solid-app-router";
-import { For, Show, createEffect } from "solid-js";
+import { For, createEffect } from "solid-js";
 import { useStore } from "../store";
 import { MessageType } from "../types/message";
 import { MessageWithAvatar, MessageWithoutAvatar } from "./Chat";
@@ -18,58 +18,54 @@ function MessagesList(props: { messages: Map<string, MessageType[]> }) {
   });
 
   return (
-    <Show when={props.messages !== undefined}>
-      <For each={props.messages ? [...props.messages] : []}>
-        {(messageGroup) => {
-          const date = messageGroup[0];
-          const messages = messageGroup[1];
-          let previousMessage = "";
-          return (
-            <>
-              <div>
-                <div class={styles.Day}>
-                  <div class={styles.DayLine} />
-                  <div class={styles.DayText}>{date}</div>
-                  <div class={styles.DayLine} />
-                </div>
+    <For each={[...props.messages]}>
+      {(messageGroup) => {
+        const date = messageGroup[0];
+        const messages = messageGroup[1];
+        let previousMessage = "";
+        return (
+          <>
+            <div>
+              <div class={styles.Day}>
+                <div class={styles.DayLine} />
+                <div class={styles.DayText}>{date}</div>
+                <div class={styles.DayLine} />
               </div>
-              <For each={messages}>
-                {(message) => {
-                  let withoutAvatar = previousMessage === message.from;
-                  previousMessage = message.from;
-                  const profile = store.profile;
-                  const activeDirect = store.directs.get(
-                    `${params.email}@gmail.com`
-                  )!;
-                  if (!profile) return null;
-                  return withoutAvatar ? (
-                    <MessageWithoutAvatar>
-                      {message.content}
-                    </MessageWithoutAvatar>
-                  ) : (
-                    <MessageWithAvatar
-                      image={
-                        activeDirect.receiver._id === message.from
-                          ? activeDirect.receiver.image
-                          : profile.image
-                      }
-                      username={
-                        activeDirect.receiver._id === message.from
-                          ? activeDirect.receiver.displayName
-                          : profile.displayName
-                      }
-                      createdAt={dayjs(message.created_at).format("HH:MM a")}
-                    >
-                      {message.content}
-                    </MessageWithAvatar>
-                  );
-                }}
-              </For>
-            </>
-          );
-        }}
-      </For>
-    </Show>
+            </div>
+            <For each={messages}>
+              {(message) => {
+                let withoutAvatar = previousMessage === message.from;
+                previousMessage = message.from;
+                const profile = store.profile;
+                const activeDirect = store.directs.get(
+                  `${params.email}@gmail.com`
+                )!;
+                if (!profile) return null;
+                return withoutAvatar ? (
+                  <MessageWithoutAvatar>{message.content}</MessageWithoutAvatar>
+                ) : (
+                  <MessageWithAvatar
+                    image={
+                      activeDirect.receiver._id === message.from
+                        ? activeDirect.receiver.image
+                        : profile.image
+                    }
+                    username={
+                      activeDirect.receiver._id === message.from
+                        ? activeDirect.receiver.displayName
+                        : profile.displayName
+                    }
+                    createdAt={dayjs(message.created_at).format("HH:MM a")}
+                  >
+                    {message.content}
+                  </MessageWithAvatar>
+                );
+              }}
+            </For>
+          </>
+        );
+      }}
+    </For>
   );
 }
 
