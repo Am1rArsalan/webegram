f96b6b91c4c6c7d9719d1f70265b68dfa919b5ca
AmirAhmadzadeh <ahamadzadeh1378@gmail.com>
2022-08-29 22:52:51 +0430

    feat: adding addMember form in members

--- src/components/Members.tsx
+++ src/components/Members.tsx
@@ -1,18 +1,53 @@
 import { classNames } from "./UI/utils/classNames";
 import styles from "./styles/Members.module.css";
+import Search from "./Search";
+import { For, ParentProps } from "solid-js";
+import { useStore } from "../store";
 
-function Members() {
+function Member(props: ParentProps) {
   return (
-    <div class={styles.Members}>
+    <div class={styles.Member}>
+      <div class={classNames(styles.MemberStatus, styles.online)} />
+      {props.children}
+    </div>
+  );
+}
+
+function Members(props: { slug: string }) {
+  const [store, { addMember }] = useStore();
+  let containerRef;
+
+  function getRoomMembers() {
+    const roomIndex = store.rooms.findIndex((room) => room.slug === props.slug);
+    if (roomIndex == -1) {
+      return [];
+    }
+
+    const room = store.rooms[roomIndex];
+
+    return room.members;
+  }
+  return (
+    <div class={styles.Members} ref={containerRef}>
       <div>
-        <div class={styles.Member}>
-          <div class={classNames(styles.MemberStatus, styles.offline)} />
-          Ryan Florence
-        </div>
-        <div class={styles.Member}>
-          <div class={classNames(styles.MemberStatus, styles.online)} />
-          cleverbot
-        </div>
+        <h5>add member form</h5>
+        <Search
+          action={(data) =>
+            addMember({
+              memberId: data._id,
+              roomSlug: props.slug,
+              memberName: data.displayName,
+            })
+          }
+          containerRef={containerRef}
+        />
+      </div>
+      <div>
+        <For each={getRoomMembers()}>
+          {(member) => {
+            return <Member>{member.displayName}</Member>;
+          }}
+        </For>
       </div>
     </div>
   );
