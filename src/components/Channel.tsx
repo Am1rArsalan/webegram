c0367bc8bbf0b7d52bae87f3ed4f801e561520b0
AmirAhmadzadeh <ahamadzadeh1378@gmail.com>
2022-08-31 10:09:22 +0430

    fix: revert to last version

--- src/components/Channel.tsx
+++ src/components/Channel.tsx
@@ -1,45 +1,47 @@
 import { useParams } from "solid-app-router";
 import { Show } from "solid-js";
 import { useStore } from "../store";
-import { MessageWithoutAvatar } from "./Chat";
+import { MessageType } from "../types/message";
 import ChatInputForm from "./ChatInputForm";
 import Members from "./Members";
 import MessagesList from "./MessagesList";
 import styles from "./styles/Chat.module.css";
 
 function Channel() {
-  const params = useParams();
-  const [store] = useStore();
+    const params = useParams();
+    const [store, { sendChannelMessage }] = useStore();
 
-  return (
-    <div class={styles.ChatContainer}>
-      <div class={styles.ChatMain}>
-        <div class={styles.ChatInfo}>
-          <div class={styles.ChannelName}>#{params.slug}</div>
-        </div>
-        <div class={styles.Messages}>
-          <div class={styles.EndOfMessages}>{"That's every message!"}</div>
-          <div>
-            <div class={styles.Day}>
-              <div class={styles.DayLine} />
-              <div class={styles.DayText}>12/6/2018</div>
-              <div class={styles.DayLine} />
+    function fetchMessages() {
+        const channel = store.rooms.find((item) => item.slug == params.slug);
+
+        if (channel) {
+            return channel.chats;
+        }
+
+        return new Map<string, MessageType[]>();
+    }
+
+    return (
+        <div class={styles.ChatContainer}>
+            <div class={styles.ChatMain}>
+                <div class={styles.ChatInfo}>
+                    <div class={styles.ChannelName}>#{params.slug}</div>
+                </div>
+                <div class={styles.Messages}>
+                    <div class={styles.EndOfMessages}>{"That's every message!"}</div>
+                    <Show when={store.rooms.find((item) => item.slug == params.slug)}>
+                        <MessagesList messages={fetchMessages()} />
+                    </Show>
+                </div>
+                <ChatInputForm
+                    sendMessage={(content) => {
+                        params.slug && sendChannelMessage(content, params.slug);
+                    }}
+                />
             </div>
-          </div>
-          <MessageWithoutAvatar> works now 1</MessageWithoutAvatar>
-          <MessageWithoutAvatar> works now 2</MessageWithoutAvatar>
-
-          <Show when={store.directs.get(`${params.email}@gmail.com`)}>
-            <MessagesList
-              messages={store.directs?.get(`${params.email}@gmail.com`)?.chats}
-            />
-          </Show>
+            <Members slug={params.slug} />
         </div>
-        <ChatInputForm />
-      </div>
-      <Members />
-    </div>
-  );
+    );
 }
 
 export default Channel;
