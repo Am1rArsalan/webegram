8c5e70727068bacb75529a1a6734e0848910940c
AmirAhmadzadeh <ahamadzadeh1378@gmail.com>
2022-09-03 23:47:04 +0430

    feat: make separate css file for add group form

--- src/components/AddChannelForm.tsx
+++ src/components/AddChannelForm.tsx
@@ -1,51 +1,46 @@
 import { createSignal, ParentProps } from 'solid-js';
 import { useStore } from '../store';
-import styles from './styles/Sidebar.module.css';
+import styles from './styles/AddChannelForm.module.css';
 import { Button } from './UI/button';
 import { Input } from './UI/input/Input';
 
 function AddChannelForm(props: { closeForm: () => void }) {
-  const [groupName, setGroupName] = createSignal('');
-  const [groupSlug, setGroupSlug] = createSignal('');
-  const [_, { createRoom }] = useStore();
+	const [groupName, setGroupName] = createSignal('');
+	const [groupSlug, setGroupSlug] = createSignal('');
+	const [_, { createRoom }] = useStore();
 
-  return (
-    <form
-      class={styles.AddGroupForm}
-      onSubmit={(ev) => {
-        ev.preventDefault();
-        const slug = groupSlug();
-        const name = groupName();
-        if (name.length < 3) return;
-        createRoom(name, slug.trim().replaceAll(/[ ]{1,}/g, '-'));
-        props.closeForm();
-      }}
-    >
-      <FormGroup label="group-name">
-        <Input value={groupName()} onChange={(ev) => setGroupName(ev.currentTarget.value)} />
-      </FormGroup>
-      <FormGroup label="group-slug">
-        <Input value={groupSlug()} onChange={(ev) => setGroupSlug(ev.currentTarget.value)} />
-      </FormGroup>
-      <Button style={{ width: '100%', height: '2rem' }} type="submit">
-        add Group
-      </Button>
-    </form>
-  );
-}
+	function onSubmit(ev: SubmitEvent) {
+		ev.preventDefault();
+		const slug = groupSlug();
+		const name = groupName();
+		if (name.length < 3) return;
+		createRoom(name, slug.trim().replaceAll(/[ ]{1,}/g, '-'));
+		props.closeForm();
+	}
 
-function FormGroup(props: ParentProps<{ label: string }>) {
-  return (
-    <div
-      style={{
-        display: 'flex',
-        'justify-content': 'space-between',
-      }}
-    >
-      <label> {props.label} </label>
-      {props.children}
-    </div>
-  );
+	return (
+		<form class={styles.AddChannelForm} onSubmit={onSubmit}>
+			<div>
+				<Input
+					class={styles.AddChannelFormInput}
+					placeholder={'Type Group Name'}
+					value={groupName()}
+					onChange={(ev) => setGroupName(ev.currentTarget.value)}
+				/>
+			</div>
+			<div>
+				<Input
+					class={styles.AddChannelFormInput}
+					placeholder={'Type Group Slug'}
+					value={groupSlug()}
+					onChange={(ev) => setGroupSlug(ev.currentTarget.value)}
+				/>
+			</div>
+			<Button class={styles.AddChannelFormSubmitButton} type="submit">
+				Add Group
+			</Button>
+		</form>
+	);
 }
 
 export default AddChannelForm;
