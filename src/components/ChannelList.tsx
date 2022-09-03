dca7e77e7c7927379828a9f535f5e3f22a72ae25
AmirAhmadzadeh <ahamadzadeh1378@gmail.com>
2022-09-04 00:07:32 +0430

    feat: update selected channel store on navigating to that

--- src/components/ChannelList.tsx
+++ src/components/ChannelList.tsx
@@ -1,23 +1,28 @@
-import { NavLink } from "solid-app-router";
-import { For } from "solid-js";
-import { useStore } from "../store";
-import styles from "./styles/Sidebar.module.css";
+import { For } from 'solid-js';
+import { NavLink } from '@solidjs/router';
+import { useStore } from '../store';
+import styles from './styles/Sidebar.module.css';
 
 function ChannelList() {
-  const [store] = useStore();
-  return (
-    <For each={store.rooms}>
-      {(room) => (
-        <NavLink
-          class={styles.ChanelLink}
-          activeClass={styles.ActiveLink}
-          href={`/channel/${room.slug}`}
-        >
-          # {room.name}
-        </NavLink>
-      )}
-    </For>
-  );
+	const [store, { updateSelectedChannel, joinRoom }] = useStore();
+	return (
+		<For each={store.rooms}>
+			{(room) => (
+				<NavLink
+					class={styles.ChanelLink}
+					activeClass={styles.ActiveLink}
+					href={`/channel/${room.slug}`}
+					replace={true}
+					onClick={() => {
+						updateSelectedChannel(room.slug);
+						joinRoom(room._id);
+					}}
+				>
+					# {room.name}
+				</NavLink>
+			)}
+		</For>
+	);
 }
 
 export default ChannelList;
