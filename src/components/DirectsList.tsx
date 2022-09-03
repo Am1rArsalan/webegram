600049b91f99fce210a78b5e1ac69ba66478c9dc
AmirAhmadzadeh <ahamadzadeh1378@gmail.com>
2022-09-04 00:04:24 +0430

    feat: update selected direct store on navigating to that

--- src/components/DirectsList.tsx
+++ src/components/DirectsList.tsx
@@ -1,12 +1,12 @@
-import { NavLink } from 'solid-app-router';
 import { For } from 'solid-js';
+import { NavLink } from '@solidjs/router';
 import { useStore } from '../store';
 import styles from './styles/UsersList.module.css';
 import { classNames } from './UI/utils/classNames';
 import UserInfo from './UserInfo';
 
 function DirectsList() {
-	const [store] = useStore();
+	const [store, { updateSelectedDirect }] = useStore();
 
 	return (
 		<ul class={styles.DirectsList}>
@@ -17,12 +17,9 @@ function DirectsList() {
 							activeClass={styles.ActiveDirectChat}
 							class={styles.DirectChat}
 							href={`/user/${direct.receiver.email.split('@')[0]}`}
+							onClick={() => updateSelectedDirect(direct.receiver.email)}
 						>
-							<UserInfo
-								//isOnline={}
-								displayName={direct.receiver.displayName}
-								image={direct.receiver.image}
-							>
+							<UserInfo displayName={direct.receiver.displayName} image={direct.receiver.image}>
 								<span
 									class={classNames(
 										styles.UserStatus,
