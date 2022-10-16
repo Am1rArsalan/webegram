ceeee2aa389f60ccfeb795bd059fed9bfde4853a
AmirAhmadzadeh <ahamadzadeh1378@gmail.com>
2022-10-16 19:43:00 +0330

    chore: eslint config

--- src/screens/Auth.tsx
+++ src/screens/Auth.tsx
@@ -2,14 +2,14 @@ import { Button } from '../components/UI/button';
 import styles from './styles/Auth.module.css';
 
 function Auth() {
-  return (
-    <div class={styles.Auth}>
-      <a href="http://localhost:8080/auth/google" class={styles.AuthButtonLink}>
-        <Button class={styles.AuthButton}> Sign In</Button>
-      </a>
-      <div class={styles.Background} />
-    </div>
-  );
+	return (
+		<div class={styles.Auth}>
+			<a href="http://localhost:8080/auth/google" class={styles.AuthButtonLink}>
+				<Button class={styles.AuthButton}> Sign In</Button>
+			</a>
+			<div class={styles.Background} />
+		</div>
+	);
 }
 
 export default Auth;
